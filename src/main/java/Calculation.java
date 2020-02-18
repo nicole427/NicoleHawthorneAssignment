d23a8739ff74a16cc24c6957a7c6cad43f9f3ead
Nicole Hawthorne <60872652+nicole427@users.noreply.github.com>
2020-02-18 23:22:45 +0200

    Initial Commit. Basic structure and simple shape classes. Tests dont pass.

---
+++ src/main/java/Calculation.java
@@ -0,0 +1,23 @@
+public class Calculation {
+    public static void main(String[] args){
+        CalculationBackEnd[] executions = new CalculationBackEnd[4];
+
+        executions[0] = create(100,200,'a');
+        executions[1] = create(200,100,'s');
+        executions[2] = create(300,26,'d');
+        executions[3] = create(100,200,'n');
+
+        for (CalculationBackEnd execution : executions){
+            execution.execute();
+            System.out.println("Result is = " + execution.getResults());
+        }
+    }
+
+    public static CalculationBackEnd create(double leftVal, double rightVal, char codes ){
+        CalculationBackEnd execution = new CalculationBackEnd();
+        execution.setLeftVal(leftVal);
+        execution.setRightVal(rightVal);
+        execution.setCodes(codes);
+        return execution;
+    }
+}
---
+++ src/main/java/CalculationBackEnd.java
@@ -0,0 +1,52 @@
+public class CalculationBackEnd {
+
+    private double leftVal;
+    private double rightVal;
+    private char codes;
+    private double results;
+    //implementing accessor and mutator
+    public double getLeftVal(){
+        return leftVal ;
+    }
+    public void setLeftVal(double leftVal){
+        this.leftVal = leftVal;
+    }
+    public double getRightVal(){
+        return rightVal ;
+    }
+    public void  setRightVal(double rightVal){
+        this.rightVal = rightVal ;
+    }
+    public char getCodes() {
+        return codes;
+    }
+    public void setCodes(char codes) {
+        this.codes = codes;
+    }
+    public double getResults(){
+        return results ;
+    }
+    public void setResults(double results){
+        this.results = results;
+    }
+
+    public void execute(){
+        switch (codes){
+            case 'a': results = leftVal + rightVal;
+                break;
+
+            case 's': results = leftVal - rightVal;
+                break;
+
+            case 'd': results = leftVal/rightVal;
+                break;
+
+            case 'n': results = leftVal * rightVal;
+                break;
+
+            default:
+                System.out.println("Invalid code, try a valid one...");
+        }
+
+    }
+}
